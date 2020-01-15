42aa600eb4a97d9bbd1999b43559053e1b638554
Luke Nemy <lukenemy7@gmail.com>
2020-01-14 17:58:39 -0800

    consuming multiple contexts with class components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import BookList from './components/Booklist';
-import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
+import React from "react";
+import Navbar from "./components/Navbar";
+import BookList from "./components/Booklist";
+import ThemeContextProvider from "./contexts/ThemeContext";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
