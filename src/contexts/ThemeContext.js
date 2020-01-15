b4d599a1d284f740b5fd83d502c659a519973451
Luke Nemy <lukenemy7@gmail.com>
2020-01-14 17:42:53 -0800

    created button to quickly change theme with context

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -6,11 +6,14 @@ class ThemeContextProvider extends Component {
   state = { 
     isLightTheme: true,
     light: {syntax: '#555', ui: '#ddd', bg: '#eee'},
-    dark: {syntax: '#ddd', ui: '#333', gb: '#555'}
+    dark: {syntax: '#ddd', ui: '#333', bg: '#555'}
+   }
+   toggleTheme = () => {
+     this.setState({ isLightTheme: !this.state.isLightTheme})
    }
   render() { 
     return ( 
-      <ThemeContext.Provider value={{...this.state}}>
+      <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme }}>
         {this.props.children}
       </ThemeContext.Provider>
     );
